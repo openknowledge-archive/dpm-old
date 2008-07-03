2b4f824aece2f8c6b8965993bd318a20e6f30c4c
rgrp <devnull@localhost>
2008-07-03 20:11:28 +0000

    [datapkg/pypkg_tools][m]: work out how to load metadata from setup.py and from PKG-INFO and use this to get current pypkgtools stuff working (i.e. extraction of metadata and name from a python distribution on disk).

--- datapkg/pypkgtools.py
+++ datapkg/pypkgtools.py
@@ -6,28 +6,50 @@ from pkg_resources import Distribution
 
 class PyPkgTools(object):
 
-    def load_distribution(self, pkg_path):
+    def load_metadata(self, pkg_path):
+        setuppy = os.path.join(pkg_path, 'setup.py')
+        if os.path.exists(setuppy):
+            import distutils.core
+            dist = distutils.core.run_setup(setuppy, stop_after='init')
+            return dist.metadata
+
+        # must be an egg distribution
         # Follows setuptools.command.easy_install.easy_install.egg_distribution.
         if os.path.isdir(pkg_path):
-            egginfo_path = os.path.join(pkg_path, 'EGG-INFO')
-            possibilities = [ egginfo_path ]
-            possibilities += [ os.path.join(pkg_path, x) for x in filter(lambda x: x.endswith('.egg-info'), os.listdir(pkg_path)) ]
-            metadata = None
-            for path in possibilities:
-                if os.path.exists(path):
-                    metadata = PathMetadata(pkg_path, path)
-                    break
-            if metadata is None:
-                raise
-                  
+            metadata_finder = PathMetadata(pkg_path,os.path.join(pkg_path,'EGG-INFO'))
         else:
-            metadata = EggMetadata(zipimport.zipimporter(pkg_path))
-        return Distribution.from_filename(pkg_path, metadata=metadata)
-	
+            metadata_finder = EggMetadata(zipimport.zipimporter(pkg_path))
+        # dist = Distribution.from_filename(egg_path,metadata=metadata_finder)
+        pkg_info = StringIO.StringIO(metadata_finder.get_metadata('PKG-INFO'))
+
+    def parse_pkg_info(self, fileobj):
+        '''Parse PKG-INFO fileobj and return DistributionMetadata object.
+        
+        task is to invert distutils.dist.DistributionMetadata.write_pkg_file
+        '''
+        # partially based on
+        # http://mail.python.org/pipermail/distutils-sig/2007-June/007783.html
+        from distutils.dist import DistributionMetadata
+        metadata = DistributionMetadata()
+        fields = metadata._METHOD_BASENAMES
+
+        import rfc822
+        messages=rfc822.Message(fileobj)
+        print messages.items()
+        for field in fields:
+            if field in ['home_page','author_email']:
+                prop=field.replace('_','-')
+            else:
+                prop=field
+            value=messages.getheader(prop)
+            # TODO: do this properly
+            # need to invert metadata._write_list
+            setattr(metadata, field, value)
+        return metadata
+
     def read_pkg_name(self, path):
-        dist = self.load_distribution(path)
-        print dist.__dict__
-        return dist.key
+        metadata = self.load_metadata(path)
+        return metadata.name
 
 
 
--- datapkg/tests/test_pypkgtools.py
+++ datapkg/tests/test_pypkgtools.py
@@ -18,11 +18,24 @@ class TestPyPkgTools(TestCase):
         self.pkg = Package(self.pkg_name)
         self.pkg.create_file_structure(self.base_path)
 
-    def test_load_distribution(self):
-        dist = self.pypkgtools.load_distribution(self.pkg_path)
+    def test_load_metadata_from_source(self):
+        dist = self.pypkgtools.load_metadata(self.pkg_path)
         assert dist
 
-    def test_read_pkg(self):
+    def test_parse_pkg_info(self):
+        pkg_info_path = os.path.join(self.pkg_path, self.pkg_name + '.egg-info',
+                'PKG-INFO')
+        meta = self.pypkgtools.parse_pkg_info(file(pkg_info_path))
+        assert meta.name == self.pkg_name
+        assert meta.version == '0.0.0'
+
+    def test_load_metadata_from_egg(self):
+        # TODO
+        # how do we find some nice egg on disk?
+        # os.system('python setup.py egg_dist')
+        pass
+
+    def test_read_pkg_name(self):
         pkg_name = self.pypkgtools.read_pkg_name(self.pkg_path)
         assert pkg_name == self.pkg_name, pkg_name
 
