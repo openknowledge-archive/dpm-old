80bc5b7fab783cf0b18af3d855ed12c78c251309
rgrp <devnull@localhost>
2009-11-26 08:59:51 +0000

    [repository][s]: (old: 2d+) add new FileRepository (using FileIndex - see cset 2 before) and refactor a bit.
      * Becoming doubtful as to value of Repository object and may deprecate soon so this isn't completely polished.

--- datapkg/repository.py
+++ datapkg/repository.py
@@ -7,13 +7,31 @@ import datapkg.index
 
 logger = logging.getLogger('datapkg.repository')
 
-class Repository(object):
+
+class StubbedRepo(object):
+    pass
+
+
+class FileRepository(object):
+    def __init__(self, repo_path):
+        self.repo_path = os.path.abspath(os.path.expanduser(repo_path))
+        self.index = datapkg.index.FileIndex(self.repo_path)
+        self.init()
+
+    def init(self):
+        if not os.path.exists(self.repo_path):
+            logger.info('Initializing repository at %s' % self.repo_path)
+            os.makedirs(self.repo_path)
+            os.makedirs(self.installed_path)
+
+
+class DbRepository(object):
     def __init__(self, repo_path):
         self.repo_path = os.path.abspath(os.path.expanduser(repo_path))
         self.installed_path = os.path.join(self.repo_path, 'installed')
         self.index_path = os.path.join(self.repo_path, 'index.db')
         self.index_dburi = 'sqlite:///%s' % self.index_path
-        self.index = datapkg.index.Index(self.index_dburi)
+        self.index = datapkg.index.DbIndex(self.index_dburi)
 
     def init(self):
         if not os.path.exists(self.repo_path):
--- datapkg/tests/test_repository.py
+++ datapkg/tests/test_repository.py
@@ -4,15 +4,26 @@ import shutil
 from StringIO import StringIO
 
 from datapkg.tests.base import TestCase
-
 import datapkg.repository
 
-class TestRepository(TestCase):
 
-    def setUp(self):
+class TestFileRepository(TestCase):
+    @classmethod
+    def setup_class(self):
         self.make_tmpdir()
         self.init_path = os.path.join(self.tmpdir, '.datapkg')
-        self.repo = datapkg.repository.Repository(self.init_path)
+
+    def test_init(self):
+        self.repo = datapkg.repository.FileRepository(self.init_path)
+        assert os.path.exists(self.init_path)
+
+
+class TestDbRepository(TestCase):
+    @classmethod
+    def setup_class(self):
+        self.make_tmpdir()
+        self.init_path = os.path.join(self.tmpdir, '.datapkg')
+        self.repo = datapkg.repository.DbRepository(self.init_path)
 
     def test_index(self):
         assert self.repo.index_dburi == 'sqlite:///%s' % self.repo.index_path
