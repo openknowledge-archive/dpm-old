85d63991e643cfb8fafe1b7efdd175600abfa371
rgrp <http://rufuspollock.org>
2010-10-11 21:45:48 +0100

    [setup][xs]: bump version to 0.7b.

--- datapkg/__init__.py
+++ datapkg/__init__.py
@@ -196,7 +196,7 @@ You can also download a distribution (only onto disk at the moment!)::
 The easiest thing (which also guarantees up-to-date-ness) is to look through
 the unit tests in ./datapkg/tests/
 '''
-__version__ = '0.7a'
+__version__ = '0.7b'
 __description__ = 'Data packaging system and utilities.'
 __description_long__ = __doc__
 __license__ = 'MIT'
