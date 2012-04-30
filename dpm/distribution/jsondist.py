f72436ea1a27f9b4084b486a25701c4ffc8bfe42
Rufus Pollock <rufus.pollock@okfn.org>
2012-04-30 04:17:49 +0100

    [distribution][s]: remove manifest stuff (unused), utilize README.dpm.* by default and do not write notes to datapackage.json as in README.

--- dpm/distribution/jsondist.py
+++ dpm/distribution/jsondist.py
@@ -14,18 +14,11 @@ class JsonDistribution(DistributionBase):
     
           * package metadata in json format.
 
-        {dist-path}/manifest.json
+        {dist-path}/data/....
 
-          * Manifest items
-
-        {dist-path}/....
-
-            # Data (and code): any files you want (specify them in the
-            # manifest).
-
+            # Data files
     '''
     metadata_filename = 'datapackage.json'
-    manifest_filename = 'manifest.json'
 
     @classmethod
     def load(self, path):
@@ -36,7 +29,6 @@ class JsonDistribution(DistributionBase):
         pkg = Package()
         pkg.installed_path = path 
         metadata_path = os.path.join(path, self.metadata_filename)
-        manifest_path = os.path.join(path, self.manifest_filename)
         fo = open(metadata_path)
         metadata = json.load(fo)
         readme_notes = self._get_notes_from_readme(path)
@@ -44,16 +36,17 @@ class JsonDistribution(DistributionBase):
             metadata['notes'] = readme_notes
         pkg.update_metadata(metadata)
         fo.close()
-        if os.path.exists(manifest_path):
-            fo = open(manifest_path)
-            pkg.manifest = json.load(fo)
-            fo.close()
         return self(pkg)
     
     @classmethod
     def _get_notes_from_readme(cls, path):
         files = [ fn for fn in os.listdir(path) if
                 fn.lower().startswith('readme') ]
+        preferred = [ fn for fn in os.listdir(path) if 
+                fn.lower().startswith('readme.dpm') ]
+        if preferred:
+            # make sure it gets used first
+            files.insert(0, preferred[0])
         if files:
             fo = open(os.path.join(path, files[0]))
             out = fo.read().decode('utf8', 'replace')
@@ -67,19 +60,18 @@ class JsonDistribution(DistributionBase):
         '''
         if not os.path.exists(path):
             os.makedirs(path)
-        metadata_path = os.path.join(path, self.metadata_filename)
-        manifest_path = os.path.join(path, self.manifest_filename)
-        fo = open(metadata_path, 'w')
-        json.dump(self.package.metadata, fo, indent=2, sort_keys=True)
-        fo.close()
-        fo = open(manifest_path, 'w')
-        json.dump(self.package.manifest, fo, indent=2, sort_keys=True)
-        fo.close()
         # write README
         notes = self.package.metadata.get('notes', '')
-        readme_fp = os.path.join(path, 'README.txt')
+        readme_fp = os.path.join(path, 'README.dpm.markdown')
         fo = open(readme_fp, 'w')
         fo.write(notes.encode('utf8'))
+        fo.close()
+        # write metadata
+        if 'notes' in self.package.metadata:
+            del self.package.metadata['notes']
+        metadata_path = os.path.join(path, self.metadata_filename)
+        fo = open(metadata_path, 'w')
+        json.dump(self.package.metadata, fo, indent=2, sort_keys=True)
         fo.close()
         # create empty 'data' directory
         datadir = os.path.join(path, 'data')
